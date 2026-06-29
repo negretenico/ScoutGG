95d40652e96df85faa453aa473da60dc43ab0185
Nico <negretenico@gmail.com>
2026-06-28 22:11:52 -0400

    adding emptyu exports

--- frontend/app/teams/[id]/page.jsx
+++ frontend/app/teams/[id]/page.jsx
@@ -1,5 +1,7 @@
 import dynamic from 'next/dynamic'
 
+export function generateStaticParams() { return [] }
+
 const TeamPage = dynamic(() => import('../../../components/TeamPage/index.jsx'), { ssr: false })
 
 export default function Page({ params }) {
